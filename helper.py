6bfbda99e0c7a369d7f850d59cae7d4546e9181e
Guyodub <guyodubjarso@gamil.com>
2022-07-20 10:55:41 +0300

    added .gitignore

--- helper.py
+++ helper.py
@@ -106,22 +106,48 @@ def assign_score_tangible_net_worth(val):
     
 """Assign scores to the adjusted working capital margin"""
 def assign_score_adjusted_working_capital(val):
-    if val <= 0.1:
+    if val >50000000:
         return 1
-    elif val <=  0.15 & val < 0.1:
+    elif val <=  50000000 and val > 10000000:
         return 2
-    elif val >= 0.2 & val < 0.15:
+    elif val <=10000000 and val > 5000000:
         return 3
-    elif val <=0.25 & val < 0.2:
+    elif val <= 5000000 and val > 1000000:
         return 4
-    elif val <=0.30 & val < 0.25:
+    elif val <= 1000000 and val > 500000:
         return 5
-    elif val <=0.35  & val < 0.3:
+    elif val <=500000  and val >250000:
         return 6
-    elif val <=0.4 & val < 0.01:
+    elif val <=250000 and val > 100000:
         return 7
     else:
         return 9
+
+"""Assign scores to the receivable days"""
+def assign_scores_receivable_days(val):
+    if val <30:
+        return 1
+    elif val >=30 & val <45:
+        return 2
+    elif val >= 45 & val < 60:
+        return 3
+    elif val >= 60& val < 75:
+        return 4
+    elif val >=75 & val < 90:
+        return 5
+    elif val >=90  & val < 105:
+        return 6
+    elif val >=105 & val < 120:
+        return 7
+    elif val >=120 & val < 150:
+        return 8
+    elif val >=150 & val < 180:
+        return 9
+    elif val >=180 & val <210:
+        return 10
+    else:
+        return 11
+
 
 """Assign scores to the inventory days"""
 def assign_scores_inventory_days(val):
@@ -196,85 +222,351 @@ def assign_scores_cash_equivalent(val):
     
 """Assign scores to current liabiities"""
 def assign_scores_current_liabilities(val):
-    if val >0.6:
+    if val >60:
         return 1
-    elif val > 0.5 & val <=0.6:
+    elif val > 45 & val <=60:
         return 2
-    elif val > 0.4 & val <= 0.5:
+    elif val > 35 & val <= 45:
         return 3
-    elif val > 0.3 & val <= 0.4:
+    elif val > 25 & val <= 35:
         return 4
-    elif val > 0.25 & val <= 0.3:
+    elif val > 20 & val <= 25:
         return 5
-    elif val > 0.2  & val <=0.25:
+    elif val > 15  & val <=20:
         return 6
-    elif val > 0.15 & val <= 0.20:
+    elif val > 10 & val <= 15:
         return 7
-    elif val > 0.1 & val <= 0.15:
+    elif val > 5 & val <= 10:
         return 8
-    elif val >=0.05 & val <= 0.1:
+    elif val >=2 & val <= 5:
+        return 9
+    else:
+        return 10
+
+
+"""Assign scores to st_debt"""
+def assign_scores_st_debt(val):
+    if val >125:
+        return 1
+    elif val > 100 & val <=125:
+        return 2
+    elif val > 80 & val <= 100:
+        return 3
+    elif val > 60 & val <= 80:
+        return 4
+    elif val > 50 & val <= 60:
+        return 5
+    elif val > 40  & val <=50:
+        return 6
+    elif val > 30  & val <= 40:
+        return 7
+    elif val > 25 & val <= 30:
+        return 8
+    elif val >=20 & val <= 25:
+        return 9
+    else:
+        return 10
+
+
+"""Assign scores to equity"""
+def assign_scores_equity(val):
+    if val >10:
+        return 1
+    elif val < 20 & val <=10:
+        return 2
+    elif val < 30 & val <= 20:
+        return 3
+    elif val < 40 & val <= 30:
+        return 4
+    elif val < 60 & val <= 40:
+        return 5
+    elif val < 60  & val <=80:
+        return 6
+    elif val < 100 & val <= 80:
+        return 7
+    elif val > 125 & val <= 100:
+        return 8
+    elif val <150 & val <= 125:
         return 9
     else:
         return 10
 
 """Assign scores to total current assets"""
 def assign_scores_total_current_assets(val):
-    if val < 0.1:
+    if val < 2.5:
         return 1
-    elif val < 0.2 & val >=0.1:
+    elif val >=2.5 and val <5:
         return 2
-    elif val < 0.3 & val >= 0.2:
+    elif val >=5 and val < 7.5:
         return 3
-    elif val > 0.4 & val >= 0.3:
+    elif val >= 7.5 and val < 10:
         return 4
-    elif val > 0.6 & val >= 0.4:
+    elif val >= 10  and val < 15:
         return 5
-    elif val > 0.8  & val <=0.6:
+    elif val >= 15  and val < 20:
         return 6
-    elif val > 1 & val <= 1.25:
+    elif val >= 20  and val < 30:
         return 7
-    elif val > 1.25 & val <= 1.5:
+    elif val >= 30 and val < 40:
         return 8
-    else:
-        return 9
-
-"""Assign scores to total liabilities"""
-def assign_scores_total_liabilities(val):
-    if val < 0.0025:
-        return 1
-    elif val < 0.005 & val >=0.0025:
-        return 2
-    elif val < 0.0075 & val >= 0.005:
-        return 3
-    elif val > 0.01 & val >= 0.0075:
-        return 4
-    elif val > 0.01 & val >= 0.015:
-        return 5
-    elif val > 0.015  & val <=0.02:
-        return 6
-    elif val > 0.02 & val <= 0.025:
-        return 7
-    elif val > 0.025 & val <= 0.03:
-        return 8
-    elif val > 0.03 & val <= 0.035:
+    elif val >= 40 and val < 50:
         return 9
     else:
         return 10
 
+"""Assign scores to net worth"""
+def assign_scores_net_worth(val):
+    if val >=0.15:
+        return 1
+    elif val >= 0.125 and val < 0.15:
+        return 2
+    elif val >= 0.15 and val < 0.1:
+        return 3
+    elif val >=0.1 and val < 0.08:
+        return 4
+    elif val >=0.06 and val < 0.08:
+        return 5
+    elif val >=0.04 and val < 0.06:
+        return 6
+    elif val >=0.04 and val < 0.02:
+        return 7
+    elif val >=0.02  and val < 0.04:
+        return 8
+    elif val >=0.01 and val < 0.02:
+        return 9
+    else:
+        return 10
+
+"""Assign scores to current ratio"""
+def assign_scores_current_ratio(val):
+    if val >2:
+        return 1
+    elif val > 1.5 and val <=2:
+        return 2
+    elif val > 1.3 and val <= 1.4:
+        return 3
+    elif val > 1.2 and val <= 1.3:
+        return 4
+    elif val > 1.1 and val <= 1.2:
+        return 5
+    elif val > 1.0 and val <=1.1:
+        return 6
+    elif val > 0.9 and val <= 1.0:
+        return 7
+    elif val > 0.8 and val <= 0.9:
+        return 8
+    elif val >=0.8 and val <= 0.5:
+        return 9
+    else:
+        return 10
+
+"""Assign scores to total debt"""
+def assign_scores_total_debt(val):
+    if val >60:
+        return 1
+    elif val > 45 & val <=60:
+        return 2
+    elif val > 35 & val <= 45:
+        return 3
+    elif val > 25 & val <= 35:
+        return 4
+    elif val > 20 & val <= 25:
+        return 5
+    elif val > 15  & val <=20:
+        return 6
+    elif val > 10 & val <= 15:
+        return 7
+    elif val > 5 & val <= 10:
+        return 8
+    elif val >=2 & val <= 5:
+        return 9
+    else:
+        return 10
+
+"""Assign scores interest expense"""
+def assign_scores_interest_expense(val):
+    if val >15:
+        return 1
+    elif val > 10 and val <=15:
+        return 2
+    elif val > 8 and val <= 10:
+        return 3
+    elif val > 7 and val <= 8:
+        return 4
+    elif val > 6 and val <= 7:
+        return 5
+    elif val > 5 and val <=6:
+        return 6
+    elif val > 4 and val <= 5:
+        return 7
+    elif val > 3 and val <= 4:
+        return 8
+    elif val >=2 and val <= 3:
+        return 9
+    else:
+        return 10
+
+"""Assign scores to net income"""
+def assign_scores_net_income(val):
+    if val >90:
+        return 1
+    elif val > 80 & val <=90:
+        return 2
+    elif val > 70 & val <= 80:
+        return 3
+    elif val > 60 & val <= 70:
+        return 4
+    elif val > 50 & val <= 60:
+        return 5
+    elif val > 40  & val <=50:
+        return 6
+    elif val > 25  & val <= 40:
+        return 7
+    elif val > 20 & val <= 25:
+        return 8
+    elif val >=15 & val <= 20:
+        return 9
+    else:
+        return 10
+
+"""Assign scores to the stability"""
+def assign_scores_stability(val):
+    if val=='outstanding':
+        return 1
+    elif val =='high':
+        return 2
+    elif val =='weak':
+        return 3
+    else:
+        return 4
+    
+"""Assign scores to economic sensitivity"""
+def assign_scores_econ_sensitivity(val):
+    if val=='outstanding':
+        return 1
+    elif val =='high':
+        return 2
+    elif val =='medium':
+        return 3
+    elif val =='weak':
+        return 4
+    else:
+        return 5
+"""Assign scores to capital intensity"""
+def assign_scores_capital_intensity(val):
+    if val=='outstanding':
+        return 1
+    elif val =='high':
+        return 2
+    elif val =='medium':
+        return 3
+    elif val =='weak':
+        return 4
+    else:
+        return 5
+
+"""Assign scores to utilization_capacity"""
+def assign_scores_utilization_capacity(val):
+    if val=='outstanding':
+        return 1
+    elif val =='high':
+        return 2
+    elif val =='medium':
+        return 3
+    elif val =='weak':
+        return 4
+    else:
+        return 5
+
+"""Assign scores to market_growth"""
+def assign_scores_market_growth(val):
+    if val=='outstanding':
+        return 1
+    elif val =='high':
+        return 2
+    elif val =='medium':
+        return 3
+    elif val =='weak':
+        return 4
+    else:
+        return 5
+"""Assign scores to how_regulated """
+def assign_scores_how_regulated(val):
+    if val=='outstanding':
+        return 1
+    elif val =='high':
+        return 2
+    elif val =='medium':
+        return 3
+    elif val =='weak':
+        return 4
+    else:
+        return 5
+"""Assign scores to how_competitive """
+def assign_scores_how_competitive(val):
+    if val=='outstanding':
+        return 1
+    elif val =='high':
+        return 2
+    elif val =='medium':
+        return 3
+    elif val =='weak':
+        return 4
+    else:
+        return 5
+
+"""Assign scores to how_diverse  """
+def assign_scores_how_diverse(val):
+    if val=='outstanding':
+        return 1
+    elif val =='high':
+        return 2
+    elif val =='medium':
+        return 3
+    elif val =='weak':
+        return 4
+    else:
+        return 5 
+
+"""Assign scores to how_mature """
+def assign_scores_how_mature(val):
+    if val=='outstanding':
+        return 1
+    elif val =='high':
+        return 2
+    elif val =='medium':
+        return 3
+    elif val =='weak':
+        return 4
+    else:
+        return 5 
+
+"""Assign scores to product_substitution """
+def assign_scores_product_substitution(val):
+    if val=='outstanding':
+        return 1
+    elif val =='high':
+        return 2
+    elif val =='medium':
+        return 3
+    elif val =='weak':
+        return 4
+    else:
+        return 5 
 
 """Assign risk classes based on the total scores"""
 def assign_risk_classes(val):
-    if val < 11:
+    if val >0.8:
         return "A"
-    elif val >=13  and  val < 15:
+    elif val >=0.6  and  val < 0.8:
         return "B"
-    elif val >=15 and val < 18:
+    elif val >=0.4 and val < 0.6:
         return "C"
-    elif val >= 18 and val < 22:
+    elif val >= 0.3 and val < 0.4:
         return "D"
-    elif val >= 22 and val < 25:
+    elif val >= 0.2 and val < 0.2:
         return "E"
-    elif val >= 25 and val <30:
+    elif val >= 0.1 and val <0.2:
         return "F"
     else:
         return "Z"
--- scoring.py
+++ scoring.py
@@ -1,42 +1,184 @@
-# importing neccesary
+ # importing neccesary
 import pandas as pd
+import pandas as pd
+import datetime
 from helper import *
 
 ## read sample data
-df = pd.read_csv("risk_scoring_mock_data.csv")
+df = pd.read_csv("risk_scoring_mock_data_guyo.csv")
 
-# transform the columns
-df['sales_growth'] = df['sales_growth']/100
-df['profit_margin'] = df['profit_margin']/100
-df['normalised_ebita_margin'] = df['normalised_ebita_margin']/100
-df['net_profit_margin'] = df['net_profit_margin']/100
-df['net_profit_margin'] = df['net_profit_margin']/100
-df['net_worth'] = df['net_worth']/100
+"""
+Write the data into the database
+"""
 
-# assign scores to the selected columns using the columns
+"""
+Read the data from the databse
+"""
+# transform columns if neccesary
+# Assign scores based on the current credit assesment system
+""" 
+Asess the  business operation.
+We use the following data points to obtain operational assessment scores;
+1. Sales growth
+2. Profit margin
+3. Normalised ebita margin
+4. Normalised profit margin
+We then assign weights to each of this to obtain an overall score for the operational assessment of the business.
+"""
+# Assign scores to the selected columns under operation_assessment scores.
 df['sales_growth_scores'] = df['sales_growth'].apply(assign_score_sales_growth)
 df['profit_margin_scores'] = df['profit_margin'].apply(assign_score_gross_profit_margin)
 df['normalised_ebita_margin_scores'] = df['normalised_ebita_margin'].apply(assign_score_noramlised_ebita_margin)
 df['net_profit_margin_scores'] = df['net_profit_margin'].apply(assign_score_net_profit_margin)
+
+# Define weights for operation_assessment scores columns
+sales_growth_wght = 0.1
+gross_profit_margin_wght = 0.2
+normalized_ebita_margin_wght = 0.35
+net_profit_margin = 0.1
+# Define the overall operation_assessment scores
+df['operational_assement_score'] = (sales_growth_wght*df['sales_growth_scores']) + (gross_profit_margin_wght*df['profit_margin_scores']) + (normalized_ebita_margin_wght*df['normalised_ebita_margin_scores']) + (net_profit_margin*df['net_profit_margin_scores'])
+
+""" 
+Assess the working capital management, liquidity & asset quality of the business.
+We use the following data points to obtain operational assessment scores;
+1. Tangible Net Worth (KES in millions)
+2. Adjusted working capital/sales
+3. Receivable days
+4. Payable days
+5. Cash equivalents
+6. Current liabilities
+7. ST debt
+We then assign weights to each of this to obtain an overall score for the operational assessment of the business.
+"""
+
 df['net_worth_scores'] = df['net_profit_margin'].apply(assign_score_tangible_net_worth)
+df['Workng_capital_scores'] = df['working_capital'].apply(assign_score_adjusted_working_capital)
+df['receivable_day_scores'] = df['receivable_day'].apply(assign_scores_receivable_days)
 df['inventory_days_scores'] = df['inventory_days'].apply(assign_scores_inventory_days)
 df['payable_days_scores'] = df['payable_days'].apply(assign_scores_inventory_days)
-df['receivable_day_scores'] = df['receivable_day'].apply(assign_scores_inventory_days)
+df['cash_equivalents_scores'] = df['cash_equivalents'].apply(assign_scores_cash_equivalent)
+df['current_liabilities_scores'] = df['current_liabilities'].apply(assign_scores_current_liabilities)
+df['st_debt_scores'] = df['st_debt'].apply(assign_scores_st_debt)
+
+## Data points weight
+net_worth_wght= 0.1
+working_capital_wght = 0.15
+receivable_days_wght = 0.1
+inventory_days_wght = 0.1
+payable_days_wght = 0.1
+cash_equivalents_wght = 0.15
+current_liability_wght = 0.15
+st_debt_wght = 0.15
+
+## score under working capital management
+df['working_capital_management_score'] = (net_worth_wght* df['net_worth_scores']) + (working_capital_wght * df['Workng_capital_scores']) + (receivable_days_wght* df['receivable_day_scores']) + (inventory_days_wght* df['inventory_days_scores']) + (payable_days_wght* df['payable_days_scores']) 
++  (cash_equivalents_wght* df['cash_equivalents_scores']) + (current_liability_wght * df['current_liabilities_scores']) +  (st_debt_wght*df['st_debt_scores'])
+
+""" 
+Assess the financing structure of the business.
+We use the following data points;
+1. Total debt/Equity
+2. ST Debt/Total current assets
+3. Net worth
+4. Current ratio
+"""
+
+df['equity_score'] = df['equity'].apply(assign_scores_equity)
+df['total_current_assets_score'] =df['total_current_assets'].apply(assign_scores_total_current_assets)
+df['net_worth_score'] = df['net_worth'].apply(assign_scores_net_worth)
+df['current_ratio_score'] = df['net_worth'].apply(assign_scores_net_worth)
+
+equity_wght= 0.33
+total_current_assets_wght = 0.22
+total_net_worth_wght = 0.33
+current_ratio_wght = 0.11
+
+df['financing_structure_score']  = (equity_wght * df['equity_score']) + (total_current_assets_wght*df['total_current_assets_score']) + (total_net_worth_wght*df['net_worth_score']) + (current_ratio_wght *df['current_ratio_score'])
+
+""" 
+Assess the cash flow & debt capacity of the business.
+We use the following data points;
+1. Ebita/total debt
+2. Interest expense
+3. Net income
+4. stability and sustainbility of cash flow
+"""
+df['total_debts_score'] = df['total_debts'].apply(assign_scores_total_debt)
+df['interest_expense_score'] =df['interest_expense'].apply(assign_scores_interest_expense)
+df['net_income_score'] = df['net_income'].apply(assign_scores_net_income)
+df['stability_score'] = df['stability'].apply(assign_scores_stability)
+
+total_debts_wght= 0.40
+interest_expense_wght = 0.4
+net_income_wght = 0.20
+stability_wght = 0.25
+
+df['cash_flow_capacity_score'] = (total_debts_wght*df['total_debts_score']) + (interest_expense_wght*df['interest_expense_score']) + (net_income_wght*df['net_income_score']) + (stability_wght*df['stability_score'])
+
+""" 
+Assess the business risk (industry/market/competitive position)
+We use the following data points;
+1. Economic sensitivity
+2. capital intensity
+3. capacity utilization
+4. Market growth
+4. How regulated
+5. How competitive
+6. How diverse
+7. How mature
+8. product substitution
+"""
+df['economic_sensitivity_score'] = df['economic_sensitivity'].apply(assign_scores_econ_sensitivity)
+df['capital_intensity_score'] = df['capital_intensity'].apply(assign_scores_capital_intensity)
+df['utilization_capacity_score'] = df['utilization_capacity'].apply(assign_scores_utilization_capacity)
+df['market_growth_score'] = df['market_growth'].apply(assign_scores_market_growth)
+df['how_regulated_score'] = df['how_regulated'].apply(assign_scores_how_regulated)
+df['how_competitive_score'] = df['how_competitive'].apply(assign_scores_how_competitive)
+df['how_diverse_score'] = df['how_diverse'].apply(assign_scores_how_diverse)
+df['how_mature_score'] = df['how_mature'].apply(assign_scores_how_mature)
+df['product_substitution_score'] =  df['product_substitution'].apply(assign_scores_product_substitution)
+
+economic_sensitivity_wght= 0.1
+capital_intensity_wght = 0.1
+utilization_capacity_wght = 0.1
+market_growth_wght = 0.1
+how_regulated_wght= 0.1
+how_competitive_wght = 0.1
+how_diverse_wght = 0.1
+how_mature_wght = 0.1
+product_substitution_wght = 0.2
+
+df['business_risk_score']  = (economic_sensitivity_wght * df['economic_sensitivity_score']) + (capital_intensity_wght*df['capital_intensity_score']) + \
+(utilization_capacity_wght * df['utilization_capacity_score']) + (market_growth_wght*df['market_growth_score']) + (how_regulated_wght*df['how_regulated_score'])  + \
+(how_competitive_wght*df['how_competitive_score']) + (how_diverse_wght*df['how_diverse_score']) + (how_mature_wght*df['how_mature_score']) + (product_substitution_wght*df['product_substitution_score'])
 
 # get a subset of data with the selected columns
-sub_df = df[['supplier_id', 'sales_growth_scores', 'profit_margin_scores',
-       'normalised_ebita_margin_scores', 'net_profit_margin_scores',
-       'net_worth_scores', 'inventory_days_scores', 'payable_days_scores',
-       'receivable_day_scores']]
-sub_df
+output = df[['supplier_id', 'operational_assement_score', 'working_capital_management_score',
+       'financing_structure_score', 'cash_flow_capacity_score','business_risk_score']]
+## Give weights to each metric
+operational_assement_wght= 0.2
+working_capital_management_wght = 0.2
+financing_structure_wght = 0.20
+cash_flow_capacity_wght = 0.25
+business_risk_wght= 0.15
 
 # get total scores
-sub_df['total_scores'] = sub_df['sales_growth_scores'] + sub_df['profit_margin_scores'] + sub_df['normalised_ebita_margin_scores'] + sub_df['net_profit_margin_scores']
-+ sub_df['net_worth_scores'] + sub_df['inventory_days_scores'] + sub_df['payable_days_scores'] + sub_df['receivable_day_scores']
-sub_df['total_scores']
+output['total_scores'] = (operational_assement_wght*output['operational_assement_score']) + (working_capital_management_wght*output['working_capital_management_score']) + \
+     (financing_structure_wght*output['financing_structure_score']) + (cash_flow_capacity_wght*output['cash_flow_capacity_score']) + (business_risk_wght*output['business_risk_score'])
 
-## assign risk classes based on risk score
-sub_df['risk_class'] = sub_df["total_scores"].apply(assign_risk_classes)
+min = output['total_scores'].min()
+max = output['total_scores'].max()
+output['total_scores_scaled'] = output['total_scores'].transform(lambda x: ((x-min) /(max-min)))
+# output['total_scores'].apply(lambda x: (x - min)/(max-min))
+# assign risk classes based on risk score
+output['risk_class'] = output["total_scores_scaled"].apply(assign_risk_classes)
+timestamp = pd.Timestamp(datetime.datetime(2021, 10, 10))
+res = timestamp.today()
+output['scoring_time'] = res
 
-## Write this into a database
+##show the data
+pd.set_option('display.max_columns', None)
+print(output.head())
+# Write this into a database
 
